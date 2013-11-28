e59f60d3d4f44165a72857a36ea4a2bffc123280
yftx.net <yftx.net@gmail.com>
2013-11-28 11:06:38 +0800

    feature: 添加移除PhoneWindow background的Demo

---
+++ src/com/github/yftx/AndroidHacks/sectionTipsAndTricks/NoBgPhoneWindow.java
@@ -0,0 +1,16 @@
+package com.github.yftx.AndroidHacks.sectionTipsAndTricks;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.github.yftx.AndroidHacks.R;
+
+/**
+ * User: Liuzl
+ * Date: 13-11-28
+ */
+public class NoBgPhoneWindow extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.no_bg_window);
+    }
+}
